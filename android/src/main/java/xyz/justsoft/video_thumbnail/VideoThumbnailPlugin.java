2576cf03596ebc23fc613426c41fd316cee06902
pbraganets <pbraganets@gmail.com>
2020-09-25 10:14:27 +0300

    Update VideoThumbnailPlugin.java

--- android/src/main/java/xyz/justsoft/video_thumbnail/VideoThumbnailPlugin.java
+++ android/src/main/java/xyz/justsoft/video_thumbnail/VideoThumbnailPlugin.java
@@ -866,7 +866,7 @@ public class VideoThumbnailPlugin implements MethodCallHandler {
         new Handler(Looper.getMainLooper()).post(runnable);
     }
     
-    private static final mFrameCapture = new AV_FrameCapture();
+    private static final AV_FrameCapture mFrameCapture = new AV_FrameCapture();
     public static Bitmap getFrameAtTimeByFrameCapture(String path, long time, int snapshot_width, int snapshot_height) {
         mFrameCapture.setDataSource(path);
         mFrameCapture.setTargetSize(snapshot_width, snapshot_height);
